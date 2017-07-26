1b5714186d7cf5f30c48e8882790f56b514416e8
Preston Landers <planders@utexas.edu>
2017-07-26 10:35:20 -0500

    Fix setup.py to not install tests/docs to the Python root.

--- setup.py
+++ setup.py
@@ -281,13 +281,14 @@ setup(name='concurrent-log-handler',
       author_email="planders@gmail.com",
       packages=['concurrent_log_handler'],
       package_dir={'': 'src', },
-      data_files=[
-          ('tests', ["stresstest.py"]),
-          ('docs', [
-              'README.md',
-              'LICENSE',
-          ]),
-      ],
+      # These aren't needed by the end user and shouldn't be installed to the Python root.
+      # data_files=[
+      #     ('tests', ["stresstest.py"]),
+      #     ('docs', [
+      #         'README.md',
+      #         'LICENSE',
+      #     ]),
+      # ],
       url="https://github.com/Preston-Landers/concurrent-log-handler",
       license="http://www.apache.org/licenses/LICENSE-2.0",
       description=doc.pop(0),
