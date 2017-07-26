e716f3ac290a46beccaefdb39f0745f15bc303d3
Preston Landers <planders@utexas.edu>
2017-07-26 10:23:34 -0500

    Fix classifier in setup.py.

--- setup.py
+++ setup.py
@@ -268,8 +268,8 @@ Programming Language :: Python
 Programming Language :: Python :: 2.6
 Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
-Classifier: Programming Language :: Python :: 3.5
-Classifier: Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
 Topic :: Software Development :: Libraries :: Python Modules
 License :: OSI Approved :: Apache Software License
 """
